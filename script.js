8f399945c2d3d919f458ffab26a0392de756c3b9
uche <gcheonwu@gmail.com>
2022-02-04 17:10:53 +0100

    adding images index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,205 @@
+
+const hover = document.querySelector('.hover');
+const mouseDown = document.querySelector('.mouse-down')
+
+const colors = document.querySelectorAll('.color');
+
+const marker = document.querySelector('.marker');
+const board = document.querySelector('.board');
+const eraser = document.getElementById('eraser');
+
+var slider = document.getElementById("myRange");
+const output = document.querySelector('.output');
+
+output.textContent = slider.value;
+var size = output.textContent; 
+
+slider.addEventListener('input', e => {
+    output.textContent = slider.value;
+    size = output.textContent;
+
+    board.innerHTML = '';
+
+    board.style = 'grid-template-columns: repeat('+ size +', 1fr)';
+
+    for(let i = 0; i != size * size; i++){
+        const div = document.createElement('div');
+        div.classList.add('boardDivs');
+        board.appendChild(div);
+    }
+
+    colors.forEach(color => {
+        color.addEventListener('click', setColor, false);
+    });
+
+    board.addEventListener('mouseover', boardColorWithMouseDown, false);
+})
+
+board.style = 'grid-template-columns: repeat('+ size +', 1fr)';
+
+for(let i = 0; i != size * size; i++){
+    const div = document.createElement('div');
+    div.classList.add('boardDivs');
+    board.appendChild(div);
+}
+
+colors.forEach(color => {
+    color.addEventListener('click', setColor, false);
+});
+
+eraser.addEventListener('click', setEraser, false);
+
+board.addEventListener('mouseover', boardColorWithMouseDown, false);
+
+var colorCanvas = document.getElementById('color_canvas');
+var ColorCtx = colorCanvas .getContext('2d');  //create 2D context for the canvas
+
+const redR = document.getElementById('rRange');
+const greenR = document.getElementById('gRange');
+const blueR = document.getElementById('bRange');
+
+var R = redR.value;
+var G = greenR.value;
+var B = blueR.value;
+const A = 1;
+
+redR.addEventListener('input', e => {
+    R = redR.value;
+    var color = `rgba(${R},${G},${B},${A})`;
+    let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+    gradientH.addColorStop(0, '#fff');
+    gradientH.addColorStop(1, color);
+    ColorCtx .fillStyle = gradientH;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+
+
+    // Create a Vertical Gradient(white to black)
+    let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
+    gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+    gradientV.addColorStop(1, '#000');
+    ColorCtx .fillStyle = gradientV;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
+    ColorCtx .canvas.height); 
+})
+
+greenR.addEventListener('input', e => {
+    G = greenR.value;
+    var color = `rgba(${R},${G},${B},${A})`;
+    let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+    gradientH.addColorStop(0, '#fff');
+    gradientH.addColorStop(1, color);
+    ColorCtx .fillStyle = gradientH;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+
+
+// Create a Vertical Gradient(white to black)
+    let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
+    gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+    gradientV.addColorStop(1, '#000');
+    ColorCtx .fillStyle = gradientV;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
+    ColorCtx .canvas.height); 
+})
+
+blueR.addEventListener('input', e => {
+    B = blueR.value;
+    var color = `rgba(${R},${G},${B},${A})`;
+    let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+    gradientH.addColorStop(0, '#fff');
+    gradientH.addColorStop(1, color);
+    ColorCtx .fillStyle = gradientH;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+
+
+// Create a Vertical Gradient(white to black)
+    let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
+    gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+    gradientV.addColorStop(1, '#000');
+    ColorCtx .fillStyle = gradientV;
+    ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
+    ColorCtx .canvas.height); 
+})
+
+var color = `rgba(${R},${G},${B},${A})`;
+let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+gradientH.addColorStop(0, '#fff');
+gradientH.addColorStop(1, color);
+ColorCtx .fillStyle = gradientH;
+ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+
+
+// Create a Vertical Gradient(white to black)
+ let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
+ gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+ gradientV.addColorStop(1, '#000');
+ ColorCtx .fillStyle = gradientV;
+ ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
+ ColorCtx .canvas.height); 
+
+colorCanvas.addEventListener('click',function(event){
+    let x = event.clientX;  // Get X coordinate
+    let y = event.clientY;  // Get Y coordinate
+    pixel = ColorCtx.getImageData(x,y,1,1)['data'];   // Read pixel Color
+    rgb = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
+    document.getElementById('pick').style = 'background-color: ' + rgb;   // Set this color to #pick background
+    document.querySelector('.picked-color').style = 'background-color: ' + rgb;
+
+    marker.style.top = event.pageY + 'px';
+    marker.style.left = event.pageX + 'px';
+}, false);
+
+
+
+function setColor(){
+    let pickedCol = window.getComputedStyle(this).backgroundColor;
+    document.querySelector('.picked-color').style = 'background-color: ' + pickedCol;
+}
+
+function setEraser(){
+    let pickedCol = 'rgb(255, 255, 255)';
+    document.querySelector('.picked-color').style = 'background-color: ' + pickedCol;
+}
+
+function boardColorWithMouseDown(){
+    
+    let col = document.querySelector('.picked-color').style.backgroundColor;
+    if(col == ''){
+        col = 'rgb(0, 0, 0)';
+    }
+    
+    let isDrawing = false;
+    
+    document.querySelectorAll('.boardDivs').forEach(element => {
+        element.addEventListener('mousedown', e => {
+            element.style = 'background-color: ' + col;
+            isDrawing = true;
+        }, false);
+    })
+
+    document.querySelectorAll('.boardDivs').forEach(element => {
+        element.addEventListener('mousemove', e => {
+            
+            if(isDrawing){
+                element.style = 'background-color: ' + col;
+            }
+        }, false)
+    })
+
+    document.querySelectorAll('.boardDivs').forEach(element => {
+        element.addEventListener('mouseup', e => {
+            
+            isDrawing = false;
+        }, false)
+    })
+}
+
+function boardColor(){
+    
+    const col = document.querySelector('.picked-color').backgroundColor;
+    
+    document.querySelectorAll('.boardDivs').forEach(element => {
+        element.addEventListener('mouseover', e => {
+            element.style = 'background-color: ' + col;
+        }, false)
+    })
+}
